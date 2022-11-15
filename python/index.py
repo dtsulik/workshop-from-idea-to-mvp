4806a06c00c12ad7db56be3c617682070ca49b2e
Giorgi Samunashvili <gsamunashvili@universe.dart.spb@gsamunashvili2.nb.ipa.dataart.net>
2022-11-15 13:19:09 +0400

    adding python

---
+++ python/index.py
@@ -0,0 +1,21 @@
+import json, boto3,os, sys, uuid
+from urllib.parse import unquote_plus
+
+s3_client = boto3.client('s3')
+
+def lambda_handler(event, context):
+    some_text = "test"
+    bucket_name = "my-tf-test-bucket-for-python-begginer"
+    file_names = ["index.html", "error.html"]
+    s3_paths = []
+    for file_name in file_names:
+        lambda_path = "/tmp/" + file_name
+        s3_paths.append ("output/" + file_name)
+        os.system('echo testing... >'+lambda_path)
+        s3 = boto3.resource("s3")
+        s3.meta.client.upload_file(lambda_path, bucket_name, file_name)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('file is created in:'+str(s3_paths))
+    }
---
+++ python/s3.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-for-python-begginer"
+  acl    = "public-read-write"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read-write"
+}
+
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": ""
+    }
+}]
+EOF
+}
